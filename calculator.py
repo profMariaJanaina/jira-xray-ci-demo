71057dd4750c99d599bf628eb9a54a95bd927eb5
profMariaJanaina <maria.ferreira05@cps.sp.gov.br>
2026-05-14 21:05:20 -0300

    First commit

---
+++ calculator.py
@@ -0,0 +1,8 @@
+def soma(a,b):
+    return a+b
+    
+def subtrai(a,b):
+    return a-b
+    
+print(soma(10,20))
+print(subtrai (5,6))
---
+++ test_calculator.py
@@ -0,0 +1,7 @@
+from calculator import soma, subtrai
+
+def test_QT8Soma():
+    assert soma(2,3) == 5
+    
+def test_QT9_subtrai():
+    assert subtrai(5,3) == 2
